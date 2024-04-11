a8114086f59be96e8be8c02a6f75018694c1e141
Gaurav <159454070+Gaurav-kumar73@users.noreply.github.com>
2024-04-11 17:49:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const togglebutton = document.querySelector('.toggle');
+const nav = document.querySelector('.nav')
+
+togglebutton.addEventListener('click', () => {
+  togglebutton.classList.toggle('active');
+  nav.classList.toggle('active');
+})
+
+
+                            
+                              
